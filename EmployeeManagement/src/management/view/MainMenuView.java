8f754f8b651256c44f60ad6967d54e13d310765e
Marc Edison Estaca <mebestaca@gmail.com>
2026-01-13 07:53:21 -0700

    main menu ui upgrade

--- EmployeeManagement/src/management/view/MainMenuView.java
+++ EmployeeManagement/src/management/view/MainMenuView.java
@@ -10,11 +10,11 @@ public class MainMenuView implements Screen{
 		System.out.println("╔═════════════════════════════╗");
 		System.out.println("║  Employee Management System ║");
 		System.out.println("╚═════════════════════════════╝");
-		System.out.println("                              ");
-		System.out.println(" [1] - Employees Menu         ");
-		System.out.println(" [2] - Departments Menu       ");
-		System.out.println("                              ");
-		System.out.println(" [3] - Exit                   ");
+		System.out.println("                               ");
+		System.out.println(" [1] - Employees Menu          ");
+		System.out.println(" [2] - Departments Menu        ");
+		System.out.println("                               ");
+		System.out.println(" [3] - Exit                    ");
 		System.out.println("");
 	}
 
