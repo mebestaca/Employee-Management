ec1dadde1e2e0b8214d1e0f61e72151d3f1d5da5
Marc Edison Estaca <mebestaca@gmail.com>
2026-01-13 07:52:48 -0700

    main menu ui upgrade

--- EmployeeManagement/src/management/view/MainMenuView.java
+++ EmployeeManagement/src/management/view/MainMenuView.java
@@ -6,16 +6,16 @@ public class MainMenuView implements Screen{
 	
 	@Override
 	public void display() {
-		System.out.println("╔══════════════════════════════╗");
-		System.out.println("║╔════════════════════════════╗║");
-		System.out.println("║║ Employee Management System ║║");
-		System.out.println("║╚════════════════════════════╝║");
-		System.out.println("║                              ║");
-		System.out.println("║ [1] - Employees Menu         ║");
-		System.out.println("║ [2] - Departments Menu       ║");
-		System.out.println("║                              ║");
-		System.out.println("║ [3] - Exit                   ║");
-		System.out.println("╚══════════════════════════════╝");
+		System.out.println("");
+		System.out.println("╔═════════════════════════════╗");
+		System.out.println("║  Employee Management System ║");
+		System.out.println("╚═════════════════════════════╝");
+		System.out.println("                              ");
+		System.out.println(" [1] - Employees Menu         ");
+		System.out.println(" [2] - Departments Menu       ");
+		System.out.println("                              ");
+		System.out.println(" [3] - Exit                   ");
+		System.out.println("");
 	}
 
 }
