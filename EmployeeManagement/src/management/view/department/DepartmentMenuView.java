2efc9162830001f6aafc177f3fb53101f7601bc0
Marc Edison Estaca <mebestaca@gmail.com>
2026-01-14 18:31:51 -0700

    department menu ui upgrade

--- EmployeeManagement/src/management/view/department/DepartmentMenuView.java
+++ EmployeeManagement/src/management/view/department/DepartmentMenuView.java
@@ -11,12 +11,12 @@ public class DepartmentMenuView implements Screen{
 		System.out.println("║       Departments Menu      ║");
 		System.out.println("╚═════════════════════════════╝");
 		System.out.println();
-		System.out.println("[1] - Display all departments");
-		System.out.println("[2] - Add a new department");
-		System.out.println("[3] - Edit existing department");
-		System.out.println("[4] - Delete a department");
+		System.out.println(" [1] - Display all departments");
+		System.out.println(" [2] - Add a new department");
+		System.out.println(" [3] - Edit existing department");
+		System.out.println(" [4] - Delete a department");
 		System.out.println();
-		System.out.println("[5] - Return to main menu");
+		System.out.println(" [5] - Return to main menu");
 		System.out.println();
 	}
 
