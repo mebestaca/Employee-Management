a0d4402f4ef8c6c4eab80b9f2e5882b510acc2b3
Marc Edison Estaca <mebestaca@gmail.com>
2026-01-15 12:35:42 -0700

    employee menu ui upgrade

--- EmployeeManagement/src/management/view/employee/EmployeeMenuView.java
+++ EmployeeManagement/src/management/view/employee/EmployeeMenuView.java
@@ -7,16 +7,16 @@ public class EmployeeMenuView implements Screen{
 	@Override
 	public void display() {
 		System.out.println();
-		System.out.println("--------------");
-		System.out.println("Employees Menu");
-		System.out.println("--------------");
+		System.out.println("╔═════════════════════════════╗");
+		System.out.println("║        Employees Menu       ║");
+		System.out.println("╚═════════════════════════════╝");
 		System.out.println();
-		System.out.println("[1] - Display all employees");
-		System.out.println("[2] - Add a new employee");
-		System.out.println("[3] - Edit existing employee");
-		System.out.println("[4] - Delete an employee");
+		System.out.println(" [1] - Display all employees");
+		System.out.println(" [2] - Add a new employee");
+		System.out.println(" [3] - Edit existing employee");
+		System.out.println(" [4] - Delete an employee");
 		System.out.println();
-		System.out.println("[5] - Return to main menu");
+		System.out.println(" [5] - Return to main menu");
 		System.out.println();
 	}
 
